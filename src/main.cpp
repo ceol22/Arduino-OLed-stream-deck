149125726f21c4c6afde3d6ec7e13d5171f2bd49
ceol22 <jordywk2004@gmail.com>
2019-03-25 16:45:09 +0100

    All buttons working + fixed delay

--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,8 @@ int button4 = 4;
 int button5 = 8;
 int button6 = 9;
 
+int interVal = 250;
+
 
 void setup() {
   Serial.begin(9600);
@@ -181,7 +183,7 @@ while (viewVal == 1) {
 
   if(button1Val == LOW){
     Joystick.setButton(1, 1);
-    delay(50);
+    delay(interVal);
     Joystick.setButton(1, 0);
    }
 
@@ -190,7 +192,7 @@ while (viewVal == 1) {
 
   if(button2Val == LOW){
     Joystick.setButton(2, 1);
-    delay(50);
+    delay(interVal);
     Joystick.setButton(2, 0);
    }
 
@@ -199,7 +201,7 @@ while (viewVal == 1) {
 
   if(button3Val == LOW){
     Joystick.setButton(3, 1);
-    delay(50);
+    delay(interVal);
     Joystick.setButton(3, 0);
    }
 
@@ -208,7 +210,7 @@ while (viewVal == 1) {
 
   if(button4Val == LOW){
     Joystick.setButton(4, 1);
-    delay(50);
+    delay(interVal);
     Joystick.setButton(4, 0);
    }
 
@@ -217,7 +219,7 @@ while (viewVal == 1) {
 
   if(button5Val == LOW){
    Joystick.setButton(5, 1);
-   delay(50);
+   delay(interVal);
    Joystick.setButton(5, 0);
     }
 
@@ -225,7 +227,7 @@ while (viewVal == 1) {
 
   if(button6Val == LOW){
     Joystick.setButton(6, 1);
-    delay(50);
+    delay(interVal);
     Joystick.setButton(6, 0);
    }
 
@@ -306,7 +308,7 @@ while (viewVal == 2) {
 
   if(button1Val == LOW){
     Joystick.setButton(7, 1);
-    delay(50);
+    delay(interVal);
     Joystick.setButton(7, 0);
    }
 
@@ -315,7 +317,7 @@ while (viewVal == 2) {
 
   if(button2Val == LOW){
     Joystick.setButton(8, 1);
-    delay(50);
+    delay(interVal);
     Joystick.setButton(8, 0);
    }
 
@@ -324,7 +326,7 @@ while (viewVal == 2) {
 
   if(button3Val == LOW){
     Joystick.setButton(9, 1);
-    delay(50);
+    delay(interVal);
     Joystick.setButton(9, 0);
    }
 
@@ -333,7 +335,7 @@ while (viewVal == 2) {
 
   if(button4Val == LOW){
     Joystick.setButton(10, 1);
-    delay(50);
+    delay(interVal);
     Joystick.setButton(10, 0);
    }
 
@@ -342,7 +344,7 @@ while (viewVal == 2) {
 
   if(button5Val == LOW){
    Joystick.setButton(11, 1);
-   delay(50);
+   delay(interVal);
    Joystick.setButton(11, 0);
     }
 
@@ -350,7 +352,7 @@ while (viewVal == 2) {
 
   if(button6Val == LOW){
     Joystick.setButton(12, 1);
-    delay(50);
+    delay(interVal);
     Joystick.setButton(12, 0);
    }
 
@@ -360,6 +362,377 @@ while (viewVal == 2) {
 // ###########################################################################################################################################
 
 
+while (viewVal == 3) {
+  int button1Val = digitalRead(button1);
+  int button2Val = digitalRead(button2);
+  int button3Val = digitalRead(button3);
+  int button4Val = digitalRead(button4);
+  int button5Val = digitalRead(button5);
+  int button6Val = digitalRead(button6);
 
+
+
+  int potVal = analogRead(A1);
+  int dispVal = map(potVal,0,1023,1,6);
+
+  switch (dispVal) {
+    case 1:
+    display.clearDisplay();
+    display.setTextSize(2);             // Normal 1:1 pixel scale
+    display.setTextColor(WHITE);        // Draw white text
+    display.setCursor(0,0);             // Start at top-left corner
+    display.println(F("Pagina 1"));
+    display.display();
+    viewVal = 1;
+    break;
+
+    case 2:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 2"));
+      display.display();
+      viewVal = 2;
+    break;
+
+    case 3:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 3"));
+      display.display();
+      viewVal = 3;
+    break;
+
+    case 4:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 4"));
+      display.display();
+      viewVal = 4;
+    break;
+
+    case 5:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 5"));
+      display.display();
+      viewVal = 5;
+    break;
+
+  }
+
+
+
+  if(button1Val == LOW){
+    Joystick.setButton(13, 1);
+    delay(interVal);
+    Joystick.setButton(13, 0);
+   }
+
+
+
+
+  if(button2Val == LOW){
+    Joystick.setButton(14, 1);
+    delay(interVal);
+    Joystick.setButton(14, 0);
+   }
+
+
+
+
+  if(button3Val == LOW){
+    Joystick.setButton(15, 1);
+    delay(interVal);
+    Joystick.setButton(15, 0);
+   }
+
+
+
+
+  if(button4Val == LOW){
+    Joystick.setButton(16, 1);
+    delay(interVal);
+    Joystick.setButton(16, 0);
+   }
+
+
+
+
+  if(button5Val == LOW){
+   Joystick.setButton(17, 1);
+   delay(interVal);
+   Joystick.setButton(17, 0);
+    }
+
+
+
+  if(button6Val == LOW){
+    Joystick.setButton(18, 1);
+    delay(interVal);
+    Joystick.setButton(18, 0);
+   }
+
+
+}
+
+
+//##################################################################################################################################################
+
+
+while (viewVal == 4) {
+  int button1Val = digitalRead(button1);
+  int button2Val = digitalRead(button2);
+  int button3Val = digitalRead(button3);
+  int button4Val = digitalRead(button4);
+  int button5Val = digitalRead(button5);
+  int button6Val = digitalRead(button6);
+
+
+
+  int potVal = analogRead(A1);
+  int dispVal = map(potVal,0,1023,1,6);
+
+  switch (dispVal) {
+    case 1:
+    display.clearDisplay();
+    display.setTextSize(2);             // Normal 1:1 pixel scale
+    display.setTextColor(WHITE);        // Draw white text
+    display.setCursor(0,0);             // Start at top-left corner
+    display.println(F("Pagina 1"));
+    display.display();
+    viewVal = 1;
+    break;
+
+    case 2:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 2"));
+      display.display();
+      viewVal = 2;
+    break;
+
+    case 3:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 3"));
+      display.display();
+      viewVal = 3;
+    break;
+
+    case 4:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 4"));
+      display.display();
+      viewVal = 4;
+    break;
+
+    case 5:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 5"));
+      display.display();
+      viewVal = 5;
+    break;
+
+  }
+
+
+
+  if(button1Val == LOW){
+    Joystick.setButton(19, 1);
+    delay(interVal);
+    Joystick.setButton(19, 0);
+   }
+
+
+
+
+  if(button2Val == LOW){
+    Joystick.setButton(20, 1);
+    delay(interVal);
+    Joystick.setButton(20, 0);
+   }
+
+
+
+
+  if(button3Val == LOW){
+    Joystick.setButton(21, 1);
+    delay(interVal);
+    Joystick.setButton(21, 0);
+   }
+
+
+
+
+  if(button4Val == LOW){
+    Joystick.setButton(22, 1);
+    delay(interVal);
+    Joystick.setButton(22, 0);
+   }
+
+
+
+
+  if(button5Val == LOW){
+   Joystick.setButton(23, 1);
+   delay(interVal);
+   Joystick.setButton(23, 0);
+    }
+
+
+
+  if(button6Val == LOW){
+    Joystick.setButton(24, 1);
+    delay(interVal);
+    Joystick.setButton(24, 0);
+   }
+
+
+}
+
+
+
+//###########################################################################################################################################
+
+
+
+while (viewVal == 5) {
+  int button1Val = digitalRead(button1);
+  int button2Val = digitalRead(button2);
+  int button3Val = digitalRead(button3);
+  int button4Val = digitalRead(button4);
+  int button5Val = digitalRead(button5);
+  int button6Val = digitalRead(button6);
+
+
+
+  int potVal = analogRead(A1);
+  int dispVal = map(potVal,0,1023,1,6);
+
+  switch (dispVal) {
+    case 1:
+    display.clearDisplay();
+    display.setTextSize(2);             // Normal 1:1 pixel scale
+    display.setTextColor(WHITE);        // Draw white text
+    display.setCursor(0,0);             // Start at top-left corner
+    display.println(F("Pagina 1"));
+    display.display();
+    viewVal = 1;
+    break;
+
+    case 2:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 2"));
+      display.display();
+      viewVal = 2;
+    break;
+
+    case 3:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 3"));
+      display.display();
+      viewVal = 3;
+    break;
+
+    case 4:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 4"));
+      display.display();
+      viewVal = 4;
+    break;
+
+    case 5:
+      display.clearDisplay();
+      display.setTextSize(2);             // Normal 1:1 pixel scale
+      display.setTextColor(WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Pagina 5"));
+      display.display();
+      viewVal = 5;
+    break;
+
+  }
+
+
+
+  if(button1Val == LOW){
+    Joystick.setButton(25, 1);
+    delay(interVal);
+    Joystick.setButton(25, 0);
+   }
+
+
+
+
+  if(button2Val == LOW){
+    Joystick.setButton(26, 1);
+    delay(interVal);
+    Joystick.setButton(26, 0);
+   }
+
+
+
+
+  if(button3Val == LOW){
+    Joystick.setButton(27, 1);
+    delay(interVal);
+    Joystick.setButton(27, 0);
+   }
+
+
+
+
+  if(button4Val == LOW){
+    Joystick.setButton(28, 1);
+    delay(interVal);
+    Joystick.setButton(28, 0);
+   }
+
+
+
+
+  if(button5Val == LOW){
+   Joystick.setButton(29, 1);
+   delay(interVal);
+   Joystick.setButton(29, 0);
+    }
+
+
+
+  if(button6Val == LOW){
+    Joystick.setButton(30, 1);
+    delay(interVal);
+    Joystick.setButton(30, 0);
+   }
+
+
+}
 
 }
